124cc1d2ae5b67fc2935cdedfbc6fce6dc27627d
Sebastian Hörl <hoerl.sebastian@googlemail.com>
2012-04-05 19:56:08 +0200

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,11 +13,13 @@ cp -r src deploy
 find vendor -name .git -type d | xargs rm -rf
 cp -r vendor deploy
 cp -r web deploy
+cp -r uploads deploy
+rm -rf deploy/uploads/*
 
-rm *.sql
+rm -rf *.sql
 app/console doctrine:migrations:migrate $migration_version --no-interaction
 app/console doctrine:migrations:migrate --write-sql
 mv *.sql deploy
-app/console doctrine:migrations:migrate
+app/console doctrine:migrations:migrate --no-interaction
 
 bin/vendors install --reinstall
